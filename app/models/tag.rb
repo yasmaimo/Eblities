3b1d3f842b0ee87f9ca3563b7cc2c8e3b7848114
yasmaimo <yasmaimo0141@gmail.com>
2018-07-08 06:07:12 +0000

    articles_view整理

---
+++ app/models/tag.rb
@@ -0,0 +1,6 @@
+class Tag < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 16,
+              message: "タグの名前は1つにつき最大14文字まで入力できます" }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180707033541) do
+ActiveRecord::Schema.define(version: 20180708055955) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
